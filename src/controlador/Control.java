3d1c06aa6b595603654754891901f010f088e2d2
juan <juan@krbosPC>
2016-10-06 01:58:19 -0300

    Confirmacion de validez de datos para creacion de personaje.

--- src/controlador/Control.java
+++ src/controlador/Control.java
@@ -9,7 +9,7 @@ public class Control {
 			int ATK=Integer.parseInt(ATKs);
 			int EVA=Integer.parseInt(EVAs);
 			int ENE=Integer.parseInt(ENEs);
-			if (HP+DEF+ATK+EVA+ENE<=200 && DEF<=80) {
+			if (HP+DEF+ATK+EVA+ENE<=200 && DEF<=20 && EVA<=80) {
 			entidades.Personaje pers= new entidades.Personaje(HP, DEF, EVA, ATK, ENE, NOM);
 			data.DBInteract basedatos = new data.DBInteract();
 			id=basedatos.guardarpers(pers);
